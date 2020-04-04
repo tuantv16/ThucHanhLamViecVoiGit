467519bdaebff9067c521119f72823838b34f13f
tuantv <vantuan24121996@gmail.com>
2020-04-04 10:10:26 +0700

    fisrt commit

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Test Vue js</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+       
+    </style>
+</head>
+<body>
+    <div id="container">
+      <header>
+           <h2>Banner website</h2>
+           <p>Đây là website đầu tiên của mình</p>
+      </header>
+       
+        <h2>Xin chào các bạn</h2>
+    </div>
+    <div id="footer">
+        <h2>Đây là Footer</h2>
+        <h2>Xin chào các bạn</h2>
+    </div>
+</body>
+</html>
